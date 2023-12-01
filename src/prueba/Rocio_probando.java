8d7b17bcb7947bd96045a91f4205b2fef2bcf1d8
rreyes5132 <151219839+rreyes5132@users.noreply.github.com>
2023-12-01 11:47:22 +0100

    Update Rocio_probando.java
    subiendo cambio

--- src/prueba/Rocio_probando.java
+++ src/prueba/Rocio_probando.java
@@ -5,6 +5,8 @@ public class Rocio_probando {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("probando");
+		System.out.println("ya está bien por hoy");
+		
 	}
 
 }
